367229d161b0a2af187bbfa9c21132322c7d6d96
Aman Kumar <58507582+Amaniitd@users.noreply.github.com>
2022-10-06 14:29:30 +0530

    Update bit_lazy.cpp

--- bit_lazy.cpp
+++ bit_lazy.cpp
@@ -1,47 +1,45 @@
-#include "bits/stdc++.h"
-using namespace std;
+// #include "bits/stdc++.h"
+// using namespace std;
 
-typedef long long ll;
-#define int long long
-
-
-
-struct bit{
-    const static int M = 200002;
-    int BIT[M] = {0};
-    bit(vector<int>& arr){
-        for (int i = 0; i < arr.size(); i++){
-            BIT[i + 1] = arr[i];
-        }
-        for (int i = 1; i <= arr.size(); i++){
-            int p = i + (i & -i);
-            if (p <= arr.size()){
-                BIT[p] += BIT[i];
-            }
-        }
-    }
-    ll sum(int i){
-        ll s = 0;
-        while (i > 0){
-            s += BIT[i];
-            i -= i & -i;
-        }
-        return s;
-    }
-    void add(int i, int k){
-        while (i < M){
-            BIT[i] += k;
-            i += i & -i;
-        }
-    }
-};
+// typedef long long ll;
+// #define int long long
 
 
-struct lazybit{
-    bit b1;
-    bit b2;
-    lazybit(vector<int>& arr){
-        b1 = bit(arr);
-        b2 = bit(arr);
-    }
-}
+
+// struct bit{
+//     const static int M = 200002;
+//     int BIT[M] = {0};
+//     bit(vector<int>& arr){
+//         for (int i = 0; i < arr.size(); i++){
+//             BIT[i + 1] = arr[i];
+//         }
+//         for (int i = 1; i <= arr.size(); i++){
+//             int p = i + (i & -i);
+//             if (p <= arr.size()){
+//                 BIT[p] += BIT[i];
+//             }
+//         }
+//     }
+//     ll sum(int i){
+//         ll s = 0;
+//         while (i > 0){
+//             s += BIT[i];
+//             i -= i & -i;
+//         }
+//         return s;
+//     }
+//     void add(int i, int k){
+//         while (i < M){
+//             BIT[i] += k;
+//             i += i & -i;
+//         }
+//     }
+// };
+
+
+// struct lazybit{
+//     bit b1;
+//     bit b2;
+//     lazybit(vector<int>& arr){
+//     }
+// }

5372a8fda2729401e4890fd991ab5b6d266718a4
Aman Kumar <58507582+Amaniitd@users.noreply.github.com>
2022-10-06 14:33:14 +0530

    Update sparse.cpp

--- sparse.cpp
+++ sparse.cpp
@@ -1,8 +1,15 @@
 
 struct sparse{
     const static int M = 200002;
-    const static int k = 22;
+    const static int K = 22;
+    vector<vector<int>> t(M, vector<int>(K));
     sparse(vector<int>& arr){
-        for(int i = 0; i < len)
+        int n = arr.size();
+        for(int i = 0; i < n; i++){
+            t[i] = arr[i];
+        }
+        for(int j = 1; j < K; j++){
+            for(int i = 0; i + (1 << j) < )
+        }
     }
 }

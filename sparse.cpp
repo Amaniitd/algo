4aafef79cfd8b93bddf3bce7627433c4045fd4bf
Aman Kumar <58507582+Amaniitd@users.noreply.github.com>
2022-10-06 14:42:36 +0530

    Update sparse.cpp

--- sparse.cpp
+++ sparse.cpp
@@ -7,7 +7,7 @@ struct sparse{
     sparse(vector<int>& arr){
         int n = arr.size();
         for(int i = 0; i < n; i++){
-            st[i] = arr[i];
+            st[i][0] = arr[i];
         }
         for(int j = 1; j < K; j++){
             for(int i = 0; i + (1 << j) <= n; i++){

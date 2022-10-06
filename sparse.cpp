aaa67ca2c00d546e7337610bc6c4905be5f3e1ef
Aman Kumar <58507582+Amaniitd@users.noreply.github.com>
2022-10-06 16:02:49 +0530

    Update sparse.cpp

--- sparse.cpp
+++ sparse.cpp
@@ -1,25 +1,25 @@
-struct sparse{
-    const static int M = 200002;
-    const static int K = 22;
-    int lg[M] = {0, 0};
+typedef long long ll;
+#define int long long
+const int K = 22;
+const int MAXN = 200002;
+int lg[MAXN + 1];
+int st[MAXN][K + 1];
 
-    int st[M][K];
-    sparse(vector<int>& arr){
-        int n = arr.size();
-        for(int i = 0; i < n; i++){
-            st[i][0] = arr[i];
-        }
-        for(int j = 1; j < K; j++){
-            for(int i = 0; i + (1 << j) <= n; i++){
-                st[i][j] = min(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
-            }
-        }
-        for(int i = 2; i <= n; i++){
-            lg[n] = lg[n/2] + 1;
-        }
-    }
-    int min(int l, int r){
-        int j = lg[r-l+1];
-        return min(st[l][j], st[r-(1<<j) + 1][j]);
-    }
-};
+
+void sparse(vector<int> & arr){
+    lg[1] = 0;
+    for (int i = 2; i <= MAXN; i++)
+        lg[i] = lg[i / 2] + 1;
+    int N = arr.size();
+    for (int i = 0; i < N; i++)
+        st[i][0] = arr[i];
+
+    for (int j = 1; j <= K; j++)
+        for (int i = 0; i + (1 << j) <= N; i++)
+            st[i][j] = min(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
+}
+
+int minm(int L, int R){ // zero indexed
+    int j = lg[R - L + 1];
+    return min(st[L][j], st[R - (1 << j) + 1][j]);
+}

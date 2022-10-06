81e72aa00ce3e1ebfdd44a7e7dfc12a214786afd
Aman Kumar <58507582+Amaniitd@users.noreply.github.com>
2022-10-06 14:34:26 +0530

    Update sparse.cpp

--- sparse.cpp
+++ sparse.cpp
@@ -9,8 +9,8 @@ struct sparse{
             t[i] = arr[i];
         }
         for(int j = 1; j < K; j++){
-            for(int i = 0; i + (1 << j) < n; i++){
-                st[i][j] = max(st[i][j-1], st[i + (1 << (j-1))])
+            for(int i = 0; i + (1 << j) <= n; i++){
+                st[i][j] = max(st[i][j-1], st[i + (1 << (j-1))][j-1])
             }
         }
     }

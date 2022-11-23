abd7ecb18ffd8c1e96d7cef4ea3d57c6db73000e
Aman Kumar <58507582+Amaniitd@users.noreply.github.com>
2022-11-24 01:32:07 +0530

    Update segment_lazy.cpp

--- segment_lazy.cpp
+++ segment_lazy.cpp
@@ -1,4 +1,16 @@
 
 #define ll long long
 
-void build(ll node)
+void build(ll node, ll start, ll end, vector<ll> arr){
+    if(start == end){
+        tree[node] = arr[start];
+        return;
+    }
+    ll mid = (start+end)/2;
+    build(2*node, start, mid, arr);
+    build(2*node+1, mid+1, end, arr);
+    return;
+}
+
+
+void update(ll node, ll start, ll end, ll pos, ll val, vec)
